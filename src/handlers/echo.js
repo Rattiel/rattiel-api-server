704beff5196bd62ae218e57c25b7c30825371703
KimJongil <whddlf0504@icloud.com>
2023-12-03 00:15:15 +0900

    테스트

--- src/handlers/echo.js
+++ src/handlers/echo.js
@@ -1,9 +1,10 @@
 // Create clients and set shared const values outside of the handler
-exports.handler = async () => {
+exports.handler = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: "hello"
+            message: "hello",
+            event: event
         }),
     }
 };
