e3ba78e23019b87ed67966235f6b4a94f6a5a1af
KimJongil <whddlf0504@icloud.com>
2023-12-03 20:42:12 +0900

    테스트

---
+++ src/handlers/get-file.js
@@ -0,0 +1,67 @@
+const {S3Client, GetObjectCommand} = require("@aws-sdk/client-s3")
+
+const BUCKET = "rattiel-storage";
+
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*'
+}
+
+const client = new S3Client({
+    region: "ap-northeast-2"
+});
+
+exports.handler = async (event) => {
+    const user = event?.requestContext?.authorizer?.claims?.sub;
+
+    if (!user) {
+        return {
+            statusCode: 400,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({
+                message: "user not authorized"
+            }),
+        }
+    }
+
+    if (!event.queryStringParameters) {
+        return {
+            statusCode: 400,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({
+                message: "bad request - need query string"
+            })
+        }
+    }
+
+    const id = event.queryStringParameters.id;
+    if (!id) {
+        return {
+            statusCode: 400,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({
+                message: "bad request - no id"
+            })
+        }
+    }
+
+    const command = new GetObjectCommand({
+        Bucket: BUCKET,
+        Key: `${user}${id}`
+    })
+
+    try {
+        const response = await client.send(command);
+
+        return {
+            statusCode: 200,
+            headers: CORS_HEADERS,
+            body: response.Body
+        }
+    } catch (error) {
+        return {
+            statusCode: 503,
+            headers: CORS_HEADERS,
+            body: JSON.stringify(error)
+        }
+    }
+};
