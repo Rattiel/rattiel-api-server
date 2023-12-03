86217df20dae8dae832dd5b80d5c10e10ff37569
KimJongil <whddlf0504@icloud.com>
2023-12-03 12:57:35 +0900

    테스트

---
+++ src/handlers/upload-file-list.js
@@ -0,0 +1,82 @@
+const {S3Client, ListObjectsV2Command, PutObjectCommand} = require("@aws-sdk/client-s3")
+
+const BUCKET = "rattiel-storage";
+
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*'
+}
+
+const client = new S3Client({
+    region: "ap-northeast-2"
+});
+
+exports.handler = async (event) => {
+    const user = event?.requestContext?.authorizer?.claims?.sub;
+
+    if (!user) {
+        return {
+            statusCode: 400,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({
+                message: "user not authorized"
+            }),
+        }
+    }
+
+    let rootDirectory = `${user}`;
+    if (event.queryStringParameters != null) {
+        return {
+            statusCode: 400,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({
+                message: "bad request - need query string"
+            })
+        }
+    }
+
+    if (event.queryStringParameters.directory) {
+        const directory = event.queryStringParameters.directory;
+        if (!directory.startsWith("/")) {
+            rootDirectory += "/";
+        }
+        rootDirectory += directory;
+        if (!directory.endsWith("/")) {
+            rootDirectory += "/";
+        }
+    }
+
+    if (!event.queryStringParameters.fileName) {
+        return {
+            statusCode: 400,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({
+                message: "bad request - need file name"
+            })
+        }
+    }
+    const fileName = event.queryStringParameters.fileName;
+
+    const command = new PutObjectCommand({
+        Bucket: BUCKET,
+        Key: `${rootDirectory}/${fileName}`,
+        Body: event.body
+    })
+
+    try {
+        const response = await client.send(command);
+
+        return {
+            statusCode: 200,
+            headers: CORS_HEADERS,
+            body: JSON.stringify({
+                message: "file uploaded"
+            })
+        }
+    } catch (error) {
+        return {
+            statusCode: 503,
+            headers: CORS_HEADERS,
+            body: JSON.stringify(error)
+        }
+    }
+};
